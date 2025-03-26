de8ff0a1d4eb5bc3c354aa579d4163e3493d90bc
Shoha12 <sohruhakubov688@gmail.com>
2025-03-26 17:47:37 +0200

    Добавляет векторную графику

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.main-nav'),
+    hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    menu.classList.toggle('main-nav--active');
+  });
+});
